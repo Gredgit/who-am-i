fc473833ed801c076e440008801c996717529861
yakasposta <yakasposta@gmail.com>
2022-05-08 01:28:48 +0300

    no message

--- Lec-3-homework/src/main/java/com/eleks/academy/whoami/networking/client/ClientPlayer.java
+++ Lec-3-homework/src/main/java/com/eleks/academy/whoami/networking/client/ClientPlayer.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
-import java.util.ArrayList;
 import java.util.List;
 
 import com.eleks.academy.whoami.core.Player;
---
+++ Lec-3-homework/src/test/java/com/eleks/academy/whoami/AppTest.java
@@ -0,0 +1,17 @@
+package com.eleks.academy.whoami;
+
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	/**
+	 * Rigorous Test :-)
+	 */
+	@Test
+	public void convertToYearsAndDaysTest() {
+		App app = new App();
+	}
+}
